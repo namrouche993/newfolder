8db4c0d626cc7997f534cc010864be21a9fc6583
namrouche993 <namrouche993@gmail.com>
2021-12-13 23:48:27 +0100

    adding files

---
+++ tests/testthat/test-strsplit1.R
@@ -0,0 +1,7 @@
+test_that("strsplit1() splits a string", {
+  expect_equal(strsplit1("a,b,c", split = ","), c("a", "b", "c"))
+})
+
+# test_that("multiplication works", {
+#   expect_equal(2 * 2, 4)
+# })
