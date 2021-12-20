1856e622004ca5f5636737ada94d9ad0b4fe7814
namrouche993 <namrouche993@gmail.com>
2021-12-20 09:14:36 +0100

    timestamp inside function

--- R/cleaning-helpers.R
+++ R/cleaning-helpers.R
@@ -1,6 +1,7 @@
 #' @import dplyr
 #' @import tidyverse
 #'
+#cat(stringi::stri_escape_unicode(x))
 lookup_table <- dplyr::tribble(
   ~where, ~english,
   "beach",     "US",
@@ -21,12 +22,13 @@ celsify_temp <- function(dat) {
   dplyr::mutate(dat, temp = dplyr::if_else(english == "US", f_to_c(temp), temp))
 }
 
-now <- Sys.time()
-timestamp <- function(time) format(time, "%Y-%B-%d_%H-%M-%S")
+# now <- Sys.time()
+# timestamp <- function(time) format(time, "%Y-%B-%d_%H-%M-%S")
 
 #' @export
-outfile_path <- function(infile) {
-  paste0(timestamp(now), "_", sub("(.*)([.]csv$)", "\\1_clean\\2", infile))
+outfile_path <- function(infile, time = Sys.time()) {
+  ts <- timestamp(time)
+  paste0(ts, "_", sub("(.*)([.]csv$)", "\\1_clean\\2", infile))
 }
 
 utils::globalVariables(c("english", "temp"))
