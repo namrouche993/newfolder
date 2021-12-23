d207f69b2a6b734756c44467f5f352bab8d87e3a
namrouche993 <namrouche993@gmail.com>
2021-12-23 14:13:31 +0100

    adding second function to run external data by run it without ::

--- R/printmtdisp.R
+++ R/printmtdisp.R
@@ -13,3 +13,14 @@ printmt0 <- function(){
 print_external_data<-function(){
   print(newfolder::dat)
 }
+
+
+#' print external data
+#'
+#' @export
+#'
+print_external_data2<-function(){
+  print(dat[1:2,1:2])
+}
+
+
