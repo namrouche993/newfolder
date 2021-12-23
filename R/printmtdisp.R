e235d899f9c66fb33acda287466f2d6f96ec6562
namrouche993 <namrouche993@gmail.com>
2021-12-23 23:21:49 +0100

    removing print_external_data2

--- R/printmtdisp.R
+++ R/printmtdisp.R
@@ -15,12 +15,13 @@ print_external_data<-function(){
 }
 
 
-#' print external data
-#'
-#' @export
-#'
-print_external_data2<-function(){
-  print(dat[1:2,1:2])
-}
-
-
+#                                           ##### THIS IS WRONG #####
+# #' print external data                    ##### THIS IS WRONG #####
+# #'                                        ##### THIS IS WRONG #####
+# #' @export                                ##### THIS IS WRONG #####
+# #'                                        ##### THIS IS WRONG #####
+# print_external_data2<-function(){         ##### THIS IS WRONG #####
+#   print(dat[1:2,1:2])                     ##### THIS IS WRONG #####
+# }                                         ##### THIS IS WRONG #####
+#                                           ##### THIS IS WRONG #####
+#                                           ##### THIS IS WRONG #####
